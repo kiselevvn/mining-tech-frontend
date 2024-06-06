796efc84666061807ba977b39b07030be0404189
vladimir kiselev <wsxxx2016@yandex.ru>
2024-06-06 13:44:09 +0300

    fix bug

--- src/pages/profile/CrearteProfile.vue
+++ src/pages/profile/CrearteProfile.vue
@@ -6,7 +6,7 @@
 
         <div class="col-10 col-md-6">
           <q-form @submit="submitForm">
-            <h1 class="text-h4">{{$route.meta.title}}</h1>
+            <h1 class="text-h4">{{ $route.meta.title }}</h1>
             <q-input
               v-model="user.username"
               type="email"
@@ -14,7 +14,7 @@
               mask="*@*"
               label="Электронная почта"
               class="q-mb-md"
-              />
+            />
             <q-input
               v-model="user.phone"
               type="tel"
@@ -22,13 +22,13 @@
               label="Телефон"
               class="q-mb-md"
               mask="+7 (###) ###-##-##"
-              />
+            />
             <q-input
               v-model="user.address"
               :outlined="true"
               label="Адрес доставки"
               class="q-mb-md"
-              />
+            />
 
             <q-input
               v-model="user.password1"
@@ -44,7 +44,7 @@
                   @click="passwordShow = !passwordShow"
                 />
               </template>
-            </q-input >
+            </q-input>
             <q-input
               v-model="user.password2"
               :type="passwordShow ? 'password' : 'text'"
@@ -59,10 +59,16 @@
                   @click="passwordShow = !passwordShow"
                 />
               </template>
-            </q-input >
+            </q-input>
             <div class="row justify-center">
               <div class="col-12 col-md-6 q-pa-xs">
-                  <q-btn color="green" class="full-width" :disable="user.password2 === '' || user.password1 !== user.password2 || disableComponentStatus"  type="submit" label="Создать учетную запись" />
+                <q-btn
+                  color="green"
+                  class="full-width"
+                  :disable="user.password2 === '' || user.password1 !== user.password2 || disableComponentStatus"
+                  type="submit"
+                  label="Создать учетную запись"
+                />
               </div>
             </div>
             <div class="row q-my-md">
@@ -70,16 +76,30 @@
               </div>
             </div>
           </q-form>
-          <q-dialog v-model="dialogErrors" position="bottom" auto-close >
-            <q-banner inline-actions class="text-white bg-red">
-            <div v-if="messages">
-            <span v-for="(m, i) in messages" :key="i">{{m}}</span><br>
-          </div>
-            <template v-slot:action>
-              <q-btn flat color="white" label="Ок" />
-            </template>
-          </q-banner>
-        </q-dialog>
+          <q-dialog
+            v-model="dialogErrors"
+            position="bottom"
+            auto-close
+          >
+            <q-banner
+              inline-actions
+              class="text-white bg-red"
+            >
+              <div v-if="messages">
+                <span
+                  v-for="(m, i) in messages"
+                  :key="i"
+                >{{ m }}</span><br>
+              </div>
+              <template v-slot:action>
+                <q-btn
+                  flat
+                  color="white"
+                  label="Ок"
+                />
+              </template>
+            </q-banner>
+          </q-dialog>
         </div>
       </div>
     </div>
@@ -91,7 +111,7 @@ import { defineComponent, ref } from 'vue'
 import useDisableCompponent from 'src/hooks/useDisableCompponent'
 export default defineComponent({
   name: 'Login',
-  setup () {
+  setup() {
     const {
       disable,
       enable,
@@ -105,24 +125,24 @@ export default defineComponent({
       dialogErrors
     }
   },
-  data () {
+  data() {
     return {
       messages: null,
       passwordShow: true,
       user: {
-        username: 'adminadmin@aadmin.ru',
+        username: '',
         phone: '',
-        address: 'ddddd',
-        password1: 'admin',
-        password2: 'admin'
+        address: '',
+        password1: '',
+        password2: ''
       }
 
     }
   },
-  created () {
+  created() {
   },
   methods: {
-    submitForm () {
+    submitForm() {
       this.disable()
       this.$api.post('api/v1/users/create/', {
         username: this.user.username,
